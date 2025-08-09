ce3103e1612428ca9d8d4caafcebcc8e5579426e
Naman Muley <naman.g.muley@gmail.com>
2025-08-09 10:00:09 +0530

    Add config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+RAW_DATA_URL = "https://huggingface.co/datasets/rajeevratan84/support-ticket-classification/resolve/main/train.csv"
+CHUNK_SIZE = 200
+CHUNK_OVERLAP = 50
+EMB_MODEL_NAME = "all-MiniLM-L6-v2"
+COLLECTION_NAME = "support_articles"
